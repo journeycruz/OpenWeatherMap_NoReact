fde22c7b954fcfd3aa572f1a355d208901dde669
Journey Cruz <journey.cruz@gmail.com>
2021-08-20 19:07:20 -0500

    Express server will now return a JSON object on path "/"

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const path = require("path");
-
 const app = express();
+const results = require('./index');
 
 let PORT = process.env.PORT || 3000;
 
@@ -9,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.send(results);
 });
 
 app.listen(PORT, function() {

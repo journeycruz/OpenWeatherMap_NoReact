2c5177420d8ea6c1607057949a852c0f6b0e84f9
Journey Cruz <journey.cruz@gmail.com>
2021-08-22 09:54:17 -0500

    Makes axios call in express server. Returns JSON object with the weather forecast for Lafayette for the next three days.

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
-const results = require('./index');
+const axios = require('axios');
 
 let PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let weatherQueryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=30.2240897&lon=-92.01984270000003&units=imperial&apikey=3ba04439f0de5f224e32e47815249bc9&cnt=3';
+
+let LafayetteForecastTemps = [];
+
 app.get("/", function(req, res) {
-    res.send(results);
+    axios
+        .get(weatherQueryURL)
+        .then(function(response) {
+            LafayetteForecastTemps.push(response.data.list[0].main);
+            LafayetteForecastTemps.push(response.data.list[1].main);
+            LafayetteForecastTemps.push(response.data.list[2].main);
+            res.send({ LafayetteForecastTemps })
+        });
 });
 
 app.listen(PORT, function() {
